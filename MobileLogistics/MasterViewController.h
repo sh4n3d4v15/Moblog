52e1395b0ad7b26bcd3d367c92f8552093ce32be
shane davis <shanedavis@shanes-MacBook-Air.local>
2014-10-23 14:43:57 +0100

    Initial Commit

---
+++ MobileLogistics/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  MobileLogistics
+//
+//  Created by shane davis on 23/10/2014.
+//  Copyright (c) 2014 shane davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ MobileLogistics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MobileLogistics
+//
+//  Created by shane davis on 23/10/2014.
+//  Copyright (c) 2014 shane davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
